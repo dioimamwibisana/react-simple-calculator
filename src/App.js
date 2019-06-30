20352420cf62fbd1ac7a4d5038076246378bd5e6
Dio Imam Wibisana <dioimamwibisana@gmail.com>
2019-06-30 18:04:45 +0700

    Add home, multiply component, add react router, and add node-sass

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+	BrowserRouter as Router,
+	NavLink,
+	Switch,
+	Route
+} from 'react-router-dom';
 
-function App() {
+import './App.scss';
+import Multiply from './components/Multiply';
+import Home from './components/Home';
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+	      <Router>
+		      <NavLink to="/">Home</NavLink>
+	        <NavLink to="/multiply">Multiply</NavLink>
+
+		      <Switch>
+			      <Route path="/multiply" component={Multiply}/>
+			      <Route path="/" component={Home} />
+			      {/*<Route path="/about" component={About}/>*/}
+			      {/*<Route path="/:user" component={User}/>*/}
+			      {/*<Route component={NoMatch}/>*/}
+		      </Switch>
+	      </Router>
+      </div>
     </div>
   );
 }
